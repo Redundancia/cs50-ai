471d2b647c628ddd06feb8a449e06cc35dda9cf3
Redundancia <hunyadiwalter94@gmail.com>
2021-12-14 04:58:09 +0100

    Minesweeper ai can win most of the time

--- week2/minesweeper/minesweeper.py
+++ week2/minesweeper/minesweeper.py
@@ -105,28 +105,34 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells
+        return set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
-
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 class MinesweeperAI():
     """
@@ -182,7 +188,46 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
+        self.moves_made.add(cell)
+        add_sentences = []
+
+        cells, count = self.new_sentence_from_move(cell, count)
+        
+        new_sentence = Sentence(cells,count)
+        add_sentences.append(new_sentence)
+
+        # Repeat this loop until we might possibly have new connections: we repeat if we add any new info anytime
+        repeat = True
+        while repeat:
+            repeat = False
+            # new_sentences is just a holder so we don't mutate our list on the flow
+            new_sentences = []
+            for add_sentence in add_sentences:
+                # Add new base knowledges
+                if add_sentence not in self.knowledge:
+                    self.knowledge.append(add_sentence)
+                    repeat = True
+                # Check if there are possible supersets in any way, if there are we add the new knowledge from it
+                for sentence in self.knowledge:
+                    if sentence.cells < add_sentence.cells:
+                        new_sentence = Sentence(set(add_sentence.cells - sentence.cells), add_sentence.count - sentence.count)
+                        if new_sentence not in self.knowledge:
+                            new_sentences.append(new_sentence)
+                            self.knowledge.append(new_sentence)
+                            repeat = True
+                    elif sentence.cells > add_sentence.cells:
+                        new_sentence = Sentence(set(sentence.cells - add_sentence.cells), sentence.count - add_sentence.count)
+                        if new_sentence not in self.knowledge:
+                            new_sentences.append(new_sentence)
+                            self.knowledge.append(new_sentence)
+                            repeat = True
+            add_sentences = new_sentences
+
+            repeat = self.update_knowledge()
+
+            self.delete_used_sentences()
+            
+
 
     def make_safe_move(self):
         """
@@ -193,7 +238,9 @@ class MinesweeperAI():
         This function may use the knowledge in self.mines, self.safes
         and self.moves_made, but should not modify any of those values.
         """
-        raise NotImplementedError
+        for move in self.safes:
+            if move not in self.moves_made:
+                return move
 
     def make_random_move(self):
         """
@@ -202,4 +249,75 @@ class MinesweeperAI():
             1) have not already been chosen, and
             2) are not known to be mines
         """
-        raise NotImplementedError
+        for row in range(self.width):
+            for column in range(self.height):
+                if (row,column) not in self.moves_made:
+                    if (row,column) not in self.mines:
+                        return (row,column)
+
+
+    def new_sentence_from_move(self,cell, count):
+        """
+        Returns a set of cells and mine count for the new sentence.
+        Only take into account cells that have not been chosen, or otherwise are not known to be safe.
+        """
+        cells = set()
+        for row_counter in range(max(0,cell[0]-1), min(cell[0]+2,self.height)):
+            for column_counter in range(max(0,cell[1]-1), min(cell[1]+2,self.width)):
+                if (row_counter,column_counter) not in self.moves_made:
+                        if (row_counter,column_counter) not in self.safes:
+                            if (row_counter,column_counter) not in self.mines:
+                                cells.add((row_counter,column_counter))
+                            else: 
+                                count -= 1
+        return (cells,count)
+
+    def update_knowledge(self):
+        """
+        Update our knowledges if possible: all mines or all safe.
+        Repeat loop until we can't update once
+        Return true if we updated at least once, false otherwise
+        """
+        did_we_update = False
+        is_knowledge_possibly_updatable = True
+
+        while is_knowledge_possibly_updatable:
+            is_knowledge_possibly_updatable = False
+            new_safes = set()
+            new_mines = set()
+            for sentence in self.knowledge:
+                if sentence.count <= 0:
+                    did_we_update = True
+                    for cell in sentence.cells:
+                        new_safes.add(cell)
+                elif sentence.count == len(sentence.cells):
+                    did_we_update = True
+                    is_knowledge_possibly_updatable = True
+                    for cell in sentence.cells:
+                        new_mines.add(cell)
+
+            for cell in new_safes:
+                self.mark_safe(cell)
+
+            for cell in new_mines:
+                self.mark_mine(cell)
+        return did_we_update
+
+    def delete_used_sentences(self):
+        """
+        If we know a sentence is safe or mines, we can delete it from knowledge.
+        This comes after we updated every other knowledge about it.
+        """
+        delete_sentences = []
+        for sentence in self.knowledge:
+            if sentence.count <= 0 or sentence.count == len(sentence.cells):
+                delete_sentences.append(sentence)
+            
+        for sentence in delete_sentences:
+            self.knowledge.remove(sentence)
+
+        new_knowledge = []
+        for sentence in self.knowledge:
+            if sentence not in new_knowledge:
+                new_knowledge.append(sentence)
+        self.knowledge = new_knowledge
