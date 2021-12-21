d3fda427976f5178cf84dda4d99ec28ba05ff59a
Redundancia <hunyadiwalter94@gmail.com>
2021-12-21 20:11:53 +0100

    Feature: iterate_pagerank

--- week3/pagerank/pagerank.py
+++ week3/pagerank/pagerank.py
@@ -128,13 +128,62 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    iterative_dictionary = {}
-    number_of_pages = len(corpus)
-    
-    # Setup starting probabilities
-    for page, sub_pages in corpus.items():
-        iterative_dictionary[page] = 1 / number_of_pages
 
+    # Return dictionary with pages: page name as key, occurence as value, value will be calculated into % before returning
+    iterative_dictionary = {}
+    number_of_pages_counted = len(corpus)
+
+    # Setup starting values, value is occurence of given page
+    for page in corpus.keys():
+        iterative_dictionary[page] = 1
+    
+    # Pick random page
+    page = random.choice(list(corpus.keys()))
+
+    # Setup starting state with random first choice
+    iterative_dictionary[page] = iterative_dictionary.get(page, 0) + 1
+    transition_probabilities = transition_model(corpus, page, damping_factor)
+    number_of_pages_counted += 1
+
+    # We will repeat until any difference in chances is bigger than 0.001
+    repeat = True
+    while repeat:
+        repeat = False
+
+        # Random number to determine which option we pick
+        random_number_to_decide_new_page = random.random()
+        probability_calculation = 0
+
+        # Iterate over the sub pages to find the random one we looking for, get the transition probabilities, increment dictionary values
+        for sub_page, probability in transition_probabilities.items():
+            probability_calculation += probability
+            # Check if random picked the given sub_page
+            if probability_calculation > random_number_to_decide_new_page:
+                
+                # Increment dict value for sub_page, these values are occurences, will be calculated into probability  
+                iterative_dictionary[sub_page] = iterative_dictionary.get(sub_page, 0) + 1
+                number_of_pages_counted += 1
+                transition_probabilities = transition_model(corpus, sub_page, damping_factor)
+                
+                # Check if any value changes by more than 0.001, if yes, repeat and break out of loop
+                for page, occurence in iterative_dictionary.items():
+                    if page == sub_page:
+                        if (occurence / number_of_pages_counted) - ((occurence - 1) / (number_of_pages_counted - 1)) >= 0.001:
+                            repeat = True
+                            break
+                    elif ((occurence / (number_of_pages_counted - 1)) - (occurence / number_of_pages_counted)) >= 0.001:
+                        repeat = True
+                        break
+                break
+
+    # Turn the dictionary values into probabilities(0-1)
+    for page, probability in iterative_dictionary.items():
+        iterative_dictionary[page] /= number_of_pages_counted
+    
+    if round(sum(iterative_dictionary.values()),4) != 1:
+        print(f"something wrong with iterate_pagerank: {round(sum(iterative_dictionary.values()),4)}")
+
+    return iterative_dictionary
 
 if __name__ == "__main__":
     main()
