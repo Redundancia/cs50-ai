97bdc57b678bde2f060f921ab45f7aba0e0db9d2
Redundancia <hunyadiwalter94@gmail.com>
2022-02-17 23:43:20 +0100

    Add update_q_value and test

--- week5/nim/test_nim.py
+++ week5/nim/test_nim.py
@@ -12,3 +12,13 @@ def test_get_q_value_valid():
     player = nim.NimAI()
     player.q[(tuple(state),action)] = 1
     assert player.get_q_value(state,action) == 1
+
+def test_update_q_value_stored():
+    state = [1,2,3]
+    action = 1
+    old_q = 0.5
+    reward = 1
+    future_rewards = 1.2
+    player = nim.NimAI()
+    player.update_q_value(state, action, old_q, reward, future_rewards) 
+    assert player.get_q_value(state,action) == old_q + player.alpha * (reward + future_rewards - old_q)
